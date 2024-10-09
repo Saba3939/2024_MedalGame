61dd040b1da00fcc71118438464e20670d2bef64
Tsujimoto <kents3939@gmaill.com>
2024-10-09 20:44:56 +0900

    feat:タイトル画面とリザルト画面など

--- Assets/Script/CameraManager.cs
+++ Assets/Script/CameraManager.cs
@@ -2,11 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 public class CameraManager : MonoBehaviour
 {
     public GameObject gameManager = null;//ゲームマネージャオブジェクトを取得
-    public float goal = 120f;//ゴールのx座標を取得
+    public float goal = 210f;//ゴールのx座標を取得
     public List<GameObject> Horse = new List<GameObject>();
     private GameManager myGameManager;//ゲームマネージャコンポーネントを定義
 
@@ -19,21 +20,33 @@ public class CameraManager : MonoBehaviour
     float min = 0, max = 0;
     void Update()
     {
+        goal = myGameManager.goalpos;//ゴールのx座標を取得  
         num = 0;
         Vector3 pos = new Vector3(transform.position.x, transform.position.y, transform.position.z);//今のポジションをposに格納
-        foreach(GameObject obj in Horse){
-            if(num==0){
+        foreach (GameObject obj in Horse)
+        {
+            if (num == 0)
+            {
                 min = obj.transform.position.x;
                 max = obj.transform.position.x;
-            }else if(min > obj.transform.position.x){
+            }
+            else if (min > obj.transform.position.x)
+            {
                 min = obj.transform.position.x;
-            }else if(max < obj.transform.position.x){
+            }
+            else if (max < obj.transform.position.x)
+            {
                 max = obj.transform.position.x;
+                myGameManager.maxhorse = obj.GetComponent<Horce>().Horsenumber;
             }
             num++;
         }
-        pos.x = ((min + max) / 2) + 2.0f;
-        if(pos.x > goal){
+        if (myGameManager.start == true)
+        {
+            pos.x = max - 10f;
+        }
+        if (pos.x > goal)
+        {
             pos.x = goal;
         }
         transform.position = pos;//posの座標に移動
